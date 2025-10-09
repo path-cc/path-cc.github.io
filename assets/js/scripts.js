1dfb25dd122104eeba525519d74c11c6529d42d8
Cannon Lock <clock@wisc.edu>
2025-10-09 12:02:13 -0500

    Add onHover for NRP comment

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -191,3 +191,6 @@ document.addEventListener("DOMContentLoaded", () => {
         filterManager.initialize();
     }
 });
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
